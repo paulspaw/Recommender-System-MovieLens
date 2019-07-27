f29b46cca0aa9bcef15f27cdb3b37bf3952d9dd1
Kyrie Lee <lzhsummer@gmail.com>
2019-07-27 15:51:02 +1000

    init some original functions of rcs.py

---
+++ rcs.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def getDataInfo(fileName):
+    # u.data: user id, movie id, rating, commend time stamp
+    return
+
+
+def getItemInfo(fileName):
+    # u.genre: movie type name, movie type id
+    # u.item: movie id, movie name, movie released date, movie type id
+    return
+
+
+def getUserInfo(fileName):
+    # u.user: user id, user gender, user occupation, user zipcode
+    return
+
+
+def recommend():
+    return
+
+
+def main():
+    recommend()
