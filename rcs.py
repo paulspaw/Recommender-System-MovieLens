fb726e3f0e6a3bb0945490255154505b0970e02e
Peng Liu <peng.paul.liu@gmail.com>
2019-07-27 21:06:07 +1000

    update dataframe function

--- rcs.py
+++ rcs.py
@@ -3,18 +3,72 @@ import pandas as pd
 
 def getDataInfo(fileName):
     # u.data: user id, movie id, rating, commend time stamp
-    return
+    df = pd.DataFrame(
+        pd.read_csv(fileName,
+                    header=None,
+                    sep='\s',
+                    names=[
+                        "user id", "movie id", "rating",
+                        "time stamp"
+                    ]))
+    return df
 
 
-def getItemInfo(fileName):
+def getItemInfo(genreFile, itemFile):
     # u.genre: movie type name, movie type id
     # u.item: movie id, movie name, movie released date, movie type id
-    return
+    df_genre = pd.DataFrame(
+        pd.read_csv(genreFile,
+                    header=None,
+                    sep='|',
+                    names=["movie type name", "movie type id"]))
+    df_item1 = pd.DataFrame(
+        pd.read_csv(itemFile,
+                    header=None,
+                    sep='|',
+                    encoding="ISO-8859-1",
+                    usecols=[0, 1, 2],
+                    names=["movie id", "movie name", "movie released date"]))
+    df_item2 = pd.DataFrame(
+        pd.read_csv(itemFile, header=None, sep='|',
+                    encoding="ISO-8859-1")).iloc[:, 5:24]
+    df_item = pd.concat([df_item1, df_item2], axis=1)
+    df_item = df_item.rename(
+        columns={
+            5: 'unknown',
+            6: 'Action',
+            7: 'Adventure',
+            8: 'Animation',
+            9: 'Children\'s\'',
+            10: 'Comedy',
+            11: 'Crime',
+            12: 'Documentary',
+            13: 'Drama',
+            14: 'Fantasy',
+            15: 'Film-Noir',
+            16: 'Horror',
+            17: 'Musical',
+            18: 'Mystery',
+            19: 'Romance',
+            20: 'Sci-Fi',
+            21: 'Thriller',
+            22: 'War',
+            23: 'Western'
+        })
+    return df_genre,df_item
 
 
 def getUserInfo(fileName):
     # u.user: user id, user gender, user occupation, user zipcode
-    return
+    df = pd.DataFrame(
+        pd.read_csv(fileName,
+                    header=None,
+                    sep='|',
+                    names=[
+                        "user id", "user gender", "user occupation",
+                        "user zipcode"
+                    ]))
+    return df
 
 
 def recommend():
@@ -23,3 +77,10 @@ def recommend():
 
 def main():
     recommend()
+
+
+if __name__ == "__main__":
+    # df_user = getUserInfo("./ml-100k/u.user")
+    # df1,df2 = getItemInfo("./ml-100k/u.genre", "./ml-100k/u.item")
+    df_rank = getDataInfo("./ml-100k/u.data")
+    print(df_rank)
