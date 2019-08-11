a516bdb436c867a910d57f1234d1f8088eeaf6fa
Peng Liu <peng.paul.liu@gmail.com>
2019-08-11 19:41:18 +1000

    update itemBasedCF

--- itemBasedCF.py
+++ itemBasedCF.py
@@ -3,7 +3,7 @@
 @Author: Peng LIU
 @Date: 2019-08-10 14:59:07
 @LastEditors: Peng LIU
-@LastEditTime: 2019-08-11 18:45:02
+@LastEditTime: 2019-08-11 19:36:29
 '''
 #coding=utf-8
 import pandas as pd
@@ -40,6 +40,14 @@ class ItemBasedCF:
         rating_num = dict()
         #物品与物品之间的相似度
         itemSimBest = dict()
+        self.movie_popular = {}
+        for user, movies in self.trainMovieDict.items():
+            for movie in movies.keys():
+                # count item popularity
+                if movie not in self.movie_popular:
+                    self.movie_popular[movie] = 0
+                self.movie_popular[movie] += 1
+        self.movie_count = len(self.movie_popular)
         
         for uid, items in self.trainMovieDict.items():
             for mid in items.keys():
@@ -94,18 +102,31 @@ class ItemBasedCF:
 
     def recallAndPrecision(self, K, N):
         hit = 0
-        recall = 0
-        precision = 0
+        rec_count = 0
+        test_count = 0
+        # varables for coverage
+        all_rec_movies = set()
+        # varables for popularity
+        popular_sum = 0
+
         for user in self.trainMovieDict.keys():
             tu = self.testMovieDict.get(user,{})
             rank = self.Recommendation(user,K,N) 
             for item,_ in rank.items():
                 if item in tu:
                     hit += 1
-            recall += len(tu)
-            precision += N
-        return (hit / (recall * 1.0),hit / (precision * 1.0))
-    
+                all_rec_movies.add(item)
+                popular_sum += math.log(1 + self.movie_popular[item])
+            test_count += len(tu)
+            rec_count += N
+            
+        precision = hit / (1.0 * rec_count)
+        recall = hit / (1.0 * test_count)
+        coverage = len(all_rec_movies) / (1.0 * self.movie_count)
+        popularity = popular_sum / (1.0 * rec_count)
+        
+        return precision, recall, coverage, popularity
+
 
 if __name__=='__main__':
     # 输入的数据集
@@ -118,11 +139,11 @@ if __name__=='__main__':
     testData = DataProcess(testFile)
     ItemCF = ItemBasedCF(trainData, testData)
     # recd = ItemCF.Recommendation(1,8,10)
+    N = 10
 
-    print("%5s%5s%20s%20s" % ('K','N',"recall",'precision'))
+    print("%5s%5s%20s%20s%20s%20s" % ('K','N','precision(%)',"recall(%)",'coverage(%)','popularity'))
     # K 选取临近的用户数量
     # N 输出推荐电影的数量
     for K in [5,10,20,40,80,160]:
-        for N in [5,10,15,20]:
-            recall,precision = ItemCF.recallAndPrecision(K,N)
-            print("%5d%5d%19.3f%%%19.3f%%" % (K,N,recall * 100,precision * 100))
+        precision,recall,coverage,popularity= ItemCF.recallAndPrecision(K,N)
+        print('%5d%5d%19.3f%19.3f%19.3f%19.3f' % (K,N,precision * 100,recall * 100,coverage * 100,popularity))
