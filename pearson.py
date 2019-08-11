f16f34031b6bf6b2f45b5f08a751b6cfb724b59d
Peng Liu <peng.paul.liu@gmail.com>
2019-08-11 23:33:10 +1000

    add requierment.txt and readme

--- pearson.py
+++ pearson.py
@@ -3,7 +3,7 @@
 @Author: Peng LIU
 @Date: 2019-08-09 14:59:00
 @LastEditors: Peng LIU
-@LastEditTime: 2019-08-11 21:52:28
+@LastEditTime: 2019-08-11 23:14:14
 '''
 import pandas as pd
 from collections import defaultdict
@@ -274,6 +274,38 @@ class UserCFPearson:
                     hit += 1
             recall += len(tu)
             precision += N
-            print((hit / (recall * 1.0),hit / (precision * 1.0)))
+            # print((hit / (recall * 1.0),hit / (precision * 1.0)))
         return (hit / (recall * 1.0),hit / (precision * 1.0))
+
+if __name__ == "__main__":
+    N = 10
+
+    # 输入的数据集
+    totalData = './ml-100k/u.data'  # 总数据集
+    trainFile = './ml-100k/u1.base'  # 训练集
+    testFile = './ml-100k/u1.test'  # 测试集
     
+    data = DataProcess(totalData)
+    trainData = DataProcess(trainFile)
+    testData = DataProcess(testFile)
+    userCF = UserCFPearson(data, trainData, testData)
+    # 根据训练集和测试集，得到预测试结果的测结果集，和测试集结果的行数一样
+    # 根据测试集和预测结果集，计算模型精确度
+    # rmse, mae = userCF.calRmseAndMae(K)
+    # print('rmse: %1.5f\t mae: %1.5f' % (rmse, mae))
+    TopN, _ = userCF.recommendation(userID=123, N=10, K=10)
+
+    i = 1
+    print('userID = 123, N = 10, K = 10')
+    
+    for line in TopN:
+        print("top", i, ": ", line[1],line[2])
+        i += 1
+
+    # # 测算recall 和 precision
+    # print("%5s%5s%20s%20s" % ('K', 'N', "recall", 'precision'))
+    # # K 选取临近的用户数量
+    # # N 输出推荐电影的数量
+    # for k in [5, 10, 20, 50, 100]:
+    #     recall, precision = userCF.recallAndPrecision(k, N)
+    #     print("%5d%5d%19.3f%%%19.3f%%" % (k,n,recall * 100,precision * 100))
